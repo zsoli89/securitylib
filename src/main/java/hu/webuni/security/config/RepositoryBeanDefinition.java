e5cdba688665aaa42854e9bc3ce75c3774f90cb2
zsoli89 <jakabzsoli89@gmail.com>
2023-08-15 19:46:40 +0200

    feat: configure bean injection

--- src/main/java/hu/webuni/security/config/RepositoryBeanDefinition.java
+++ src/main/java/hu/webuni/security/config/RepositoryBeanDefinition.java
@@ -1,6 +1,5 @@
 package hu.webuni.security.config;
 
-import org.antlr.v4.runtime.misc.NotNull;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.boot.autoconfigure.AutoConfigurationPackages;
 import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
@@ -11,7 +10,7 @@ import hu.webuni.security.repository.ResponsibilityAppUserRepository;
 public class RepositoryBeanDefinition implements ImportBeanDefinitionRegistrar {
 
     @Override
-    public void registerBeanDefinitions(@NotNull AnnotationMetadata importingClassMetadata, @NotNull BeanDefinitionRegistry registry) {
+    public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata, BeanDefinitionRegistry registry) {
         AutoConfigurationPackages.register(registry, AppUserRepository.class.getPackageName());
         AutoConfigurationPackages.register(registry, ResponsibilityAppUserRepository.class.getPackageName());
     }
