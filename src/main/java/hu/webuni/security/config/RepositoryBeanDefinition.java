125b619d444946a29ca71a560468797db78f2c98
zsoli89 <jakabzsoli89@gmail.com>
2023-08-15 19:14:01 +0200

    fix: import repository as a Bean

--- src/main/java/hu/webuni/security/config/RepositoryBeanDefinition.java
+++ src/main/java/hu/webuni/security/config/RepositoryBeanDefinition.java
@@ -1,10 +1,12 @@
-package hu.webuni.security.repository;
+package hu.webuni.security.config;
 
 import org.antlr.v4.runtime.misc.NotNull;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.boot.autoconfigure.AutoConfigurationPackages;
 import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
 import org.springframework.core.type.AnnotationMetadata;
+import hu.webuni.security.repository.AppUserRepository;
+import hu.webuni.security.repository.ResponsibilityAppUserRepository;
 
 public class RepositoryBeanDefinition implements ImportBeanDefinitionRegistrar {
 
