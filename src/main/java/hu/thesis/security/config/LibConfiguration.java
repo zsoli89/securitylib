76635dfddccc575041973dccaa1cd11067876c66
zsoli <jakabzsoli89@gmail.com>
2023-11-07 21:39:16 +0100

    feat: bean injection

--- src/main/java/hu/thesis/security/config/LibConfiguration.java
+++ src/main/java/hu/thesis/security/config/LibConfiguration.java
@@ -1,29 +1,29 @@
-//package hu.thesis.security.config;
-//
-//import hu.thesis.security.JwtAuthFilter;
-//import hu.thesis.security.JwtTokenService;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//@ComponentScan(basePackages = {"hu.thesis.security"})
-//public class LibConfiguration {
-//
-//    private final JwtTokenService jwtTokenService;
-//
-//    public LibConfiguration(JwtTokenService jwtTokenService) {
-//        this.jwtTokenService = jwtTokenService;
-//    }
-//
-//    @Bean
-//    public JwtTokenService jwtTokenService() {
-//        return new JwtTokenService();
-//    }
-//
-//    @Bean
-//    public JwtAuthFilter jwtAuthFilter() {
-//        return new JwtAuthFilter(jwtTokenService);
-//    }
-//
-//}
+package hu.thesis.security.config;
+
+import hu.thesis.security.JwtAuthFilter;
+import hu.thesis.security.JwtTokenService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"hu.thesis.security"})
+public class LibConfiguration {
+
+    private final JwtTokenService jwtTokenService;
+
+    public LibConfiguration(JwtTokenService jwtTokenService) {
+        this.jwtTokenService = jwtTokenService;
+    }
+
+    @Bean
+    public JwtTokenService jwtTokenService() {
+        return new JwtTokenService();
+    }
+
+    @Bean
+    public JwtAuthFilter jwtAuthFilter() {
+        return new JwtAuthFilter(jwtTokenService);
+    }
+
+}
